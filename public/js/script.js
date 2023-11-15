c58a44fde48004fbb5c14a7685e1a83e290b3391
tomrcc <tom@cloudcannon.com>
2023-11-15 14:07:38 +1300

    Tidy script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,70 +1,46 @@
 $(document).ready(function () {
-    "use strict";
-    // Magnific Popup
-    $('.popup-vimeo').each(function(i, obj) {
-      $(this).magnificPopup({
-        disableOn: 700,
-        type: "iframe",
-        mainClass: "mfp-fade",
-        removalDelay: 160,
-        preloader: false,
-        fixedContentPos: false,
-      });
+  $(window).on('scroll', function () {
+    var windscroll = $(window).scrollTop();
+    if (windscroll >= 70) {
+      $('#mainnavigationBar').addClass('sticky-nav');
+    } else {
+      $('#mainnavigationBar').removeClass('sticky-nav');
+    }
+  });
+  $('.navbar-toggler').on('click', function () {
+    var navbar = $('#mainnavigationBar');
+    navbar.toggleClass('bg-nav');
   });
 
-    $(window).on("scroll", function () {
-      var windscroll = $(window).scrollTop();
-      if (windscroll >= 70) {
-        $("#mainnavigationBar").addClass("sticky-nav");
-      } else {
-        $("#mainnavigationBar").removeClass("sticky-nav");
-      }
-    });
-    $(".navbar-toggler").on("click", function () {
-      var navbar = $("#mainnavigationBar");
-      navbar.toggleClass("bg-nav");
-    });
-  
-    //Show password
-    $(".viewPassword").click(function () {
-      $(this).toggleClass("fa-eye fa-eye-slash");
-      var input = $($(this).attr("toggle"));
-      if (input.attr("type") == "password") {
-        input.attr("type", "text");
-      } else {
-        input.attr("type", "password");
-      }
-    });
-    // Scroll spy style start
-    $(".nav-link").click(function () {
-      $(".nav-link").removeClass("active");
-      $(this).addClass("active");
-    });
-    // Scroll spy
-    $(document).ready(function () {
-      if(document.getElementById("#scrol-nav")){
-        $("body").scrollspy({
-          target: "#scrol-nav",
-          offset: 20,
-        });
-    
-        // Navbar fade
+  // Scroll spy style start
+  $('.nav-link').click(function () {
+    $('.nav-link').removeClass('active');
+    $(this).addClass('active');
+  });
+  // Scroll spy
+  $(document).ready(function () {
+    if (document.getElementById('#scrol-nav')) {
+      $('body').scrollspy({
+        target: '#scrol-nav',
+        offset: 20,
+      });
+
+      // Navbar fade
+      changeNavbar();
+
+      $(window).scroll(function () {
         changeNavbar();
-    
-        $(window).scroll(function () {
-          changeNavbar();
-        });
-    
-        function changeNavbar() {
-          var navbar = $("#scrol-nav");
-          if ($(this).scrollTop() >= 20) {
-            navbar.addClass("bg-light").removeClass("bg-transparent");
-          } else if ($(this).scrollTop() < 20) {
-            navbar.removeClass("bg-light").addClass("bg-transparent");
-          }
+      });
+
+      function changeNavbar() {
+        var navbar = $('#scrol-nav');
+        if ($(this).scrollTop() >= 20) {
+          navbar.addClass('bg-light').removeClass('bg-transparent');
+        } else if ($(this).scrollTop() < 20) {
+          navbar.removeClass('bg-light').addClass('bg-transparent');
         }
       }
-    });
-    //end  Scroll spy style
+    }
   });
-  
+  //end  Scroll spy style
+});
