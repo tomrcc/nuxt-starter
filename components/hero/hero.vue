7d621ecc506bee051b183f0a924e342503863dd3
tomrcc <tom@cloudcannon.com>
2023-11-15 14:17:24 +1300

    Removing sendit components

---
+++ components/hero/hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <section :data-cms-bind="dataBinding">
+    <h1>{{ block.title }}</h1>
+    <div>
+      <p v-html="block.description"></p>
+    </div>
+    <a :href="block.button.link">
+      {{ block.button.text }}
+    </a>
+    <img
+      format="webp"
+      :src="block.image"
+      alt="hero-two-images"
+      v-if="block.image"
+    />
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['block', 'dataBinding'],
+};
+</script>
